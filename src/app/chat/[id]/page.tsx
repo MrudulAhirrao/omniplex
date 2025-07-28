1cc38e03ce60c8e656a469cddaaa6150610cf67b
DELL <ahiraomrudul@gmail.com>
2025-07-28 21:45:58 +0530

    updated files

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -4,6 +4,7 @@ import { Metadata } from "next";
 
 type Props = {
   params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
 };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
